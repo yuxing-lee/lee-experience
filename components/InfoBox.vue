f59cd2f6d5820d51da1c0e47d0a567019e45b1d7
YUXING-LEE <leo832232@gmail.com>
2024-05-13 21:02:56 +0800

    feat: add experience's detail and component

---
+++ components/InfoBox.vue
@@ -0,0 +1,34 @@
+<template>
+    <article
+        class="flex flex-col justify-center items-center w-full h-60 p-8 bg-gradient-to-b from-sky-800 to-sky-500 text-gray-100 break-words ease-linear duration-150 hover:to-gray-800 hover:scale-105"
+        v-on:mouseover="hover = true"
+        v-on:mouseleave="hover = false"
+    >
+        <p class="font-bold text-sm mb-5 lg:text-xl">{{ title }}</p>
+        <p>{{ description }}</p>
+        <a
+            class="animate-bounce absolute bottom-3"
+            v-show="hover"
+            :href="'#' + tag"
+        >
+            <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-10 h-10"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+            </svg>
+        </a>
+    </article>
+</template>
+
+<script setup>
+defineProps({ title: String, description: String, tag: String });
+let hover = ref(false);
+</script>
--- pages/experience.vue
+++ pages/experience.vue
@@ -1,21 +1,41 @@
 <template>
-    <div class="flex justify-around items-center w-full">
-        <Timeline
-            class="duration-300 ease-in origin-right"
-            :class="{ 'opacity-0': !visible }"
-            title="高雄科技大學-人工智慧產業化應用研究中心"
-            description="軟體工程師"
-        ></Timeline>
-        <div class="duration-1000 ease-in" :class="{ 'opacity-0': !visible }">
-            詳細資訊
+    <div>
+        <div class="flex w-full h-40 justify-center items-center lg:h-96">
+            <p class="text-xl lg:text-5xl">
+                高雄科技大學 - 人工智慧產業化應用研究中心
+            </p>
+        </div>
+        <div
+            class="grid grid-cols-1 gap-4 container mx-auto px-4 lg:grid-cols-3"
+        >
+            <InfoBox
+                v-for="project in projects"
+                :title="project.title"
+                :description="project.description"
+                :tag="project.tag"
+            />
         </div>
     </div>
 </template>
 
 <script setup>
-let visible = ref(false);
-
-setTimeout(() => {
-    visible.value = true;
-}, 200);
+let projects = [
+    {
+        title: "車流辨識系統",
+        description:
+            "能夠在不同環境下進行車流追蹤與辨識，並針對不同場域快速搭建車流系統",
+        tag: "",
+    },
+    {
+        title: "車流管理系統",
+        description: "收集各節點端資料，進行圖形化的管理，監控當前節點運作狀態",
+        tag: "",
+    },
+    {
+        title: "5G OAM平台",
+        description:
+            "視覺化及管理介面前後端開發，使用簡單的介面操作，即可快速建構5G網路及管理",
+        tag: "",
+    },
+];
 </script>
